f454ca4e5cdbdb553c4fcd1fa7469a24d55a30b5
Juan19450169 <l19450169@itz.edu.mx>
2024-12-18 01:04:50 -0600

    .env

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
-app.use('/img/', express.static(path.join(__dirname, '/public/img/')))
+app.use('/img/', express.static(path.join(__dirname, '/public/img/')));
 
 //Indicamos que el servidor 
 app.use('/api/', authRoutes);
